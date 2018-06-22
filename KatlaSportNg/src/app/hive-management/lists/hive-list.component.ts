e82065caf4245c6431eccc3e61cbabff114b4a78
YAUHENI MASARNOUSKI <jeniamasarn94@gmail.com>
2018-06-22 13:08:25 +0300

    Add button Restore to angular view HiveListComponent with action onRestore. Add action onRestore to HiveListComponent. Add setHiveStatus method to HiveService. Add buttons Delete and Undelete to angular view HiveSectionListComponent with actions onDelete and onUndelete accordingly. Add actions onDelete and onUndelete to HiceSectionListComponent.Add setHiveSectionStatus method to HiveSectionService

--- KatlaSportNg/src/app/hive-management/lists/hive-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-list.component.ts
@@ -7,6 +7,7 @@ import { HiveService } from '../services/hive.service';
   templateUrl: './hive-list.component.html',
   styleUrls: ['./hive-list.component.css']
 })
+
 export class HiveListComponent implements OnInit {
 
   hives: HiveListItem[];
@@ -27,5 +28,7 @@ export class HiveListComponent implements OnInit {
   }
 
   onRestore(hiveId: number) {
+    var hive = this.hives.find(h => h.id == hiveId);
+    this.hiveService.setHiveStatus(hiveId, false).subscribe(c => hive.isDeleted = false);
   }
 }
--- KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HiveSectionListItem } from '../models/hive-section-list-item';
 import { HiveService } from '../services/hive.service';
+import { HiveSectionService } from '../services/hive-section.service'
+import { CodegenComponentFactoryResolver } from '@angular/core/src/linker/component_factory_resolver';
 
 @Component({
   selector: 'app-hive-section-list',
@@ -17,6 +19,7 @@ export class HiveSectionListComponent implements OnInit {
     private route: ActivatedRoute,
     public router: Router,
     private hiveService: HiveService,
+    private hiveSectionService:HiveSectionService
   ) { }
 
   ngOnInit() {
@@ -25,4 +28,17 @@ export class HiveSectionListComponent implements OnInit {
       this.hiveService.getHiveSections(this.hiveId).subscribe(s => this.hiveSections = s);
     })
   }
+
+  onDelete(sectionId:number)
+  {
+      var section = this.hiveSections.find(h => h.id == sectionId);
+      this.hiveSectionService.setHiveSectionStatus(sectionId, true).subscribe(c => section.isDeleted = true);
+  }
+
+  onUndelete(sectionId:number)
+  {
+    var section = this.hiveSections.find(h => h.id == sectionId);
+    this.hiveSectionService.setHiveSectionStatus(sectionId, false).subscribe(c => section.isDeleted = false);
+  }
+
 }
